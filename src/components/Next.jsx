25298d1bf235336135e90f8d6c0f4291ec6d9165
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-29 01:49:21 -0400

    Typo !

--- src/components/Next.jsx
+++ src/components/Next.jsx
@@ -4,7 +4,7 @@ function Next() {
     return (
         <div className="p-[5%]">
             <h3 className="text-3xl font-bold underline underline-offset-2 decoration-violet-500">What's Next</h3>
-            <ul className="p-[5%] text-xl list-disc space-y-8">
+            <ul className="px-[5%] py-5 text-xl list-disc space-y-8">
                 <li>In the future, we aim to make DrowsyDriver accessible to everyone. With more time, we envision developing a compact device that can be easily mounted on dashboards, catering to concerned family members, friends, and truck drivers alike.</li>
                 <li>Additionally, we plan to gather data from this device to create an application that alerts family members about their loved ones’ safety. Our goal is to enhance road safety and ensure peace of mind for all users.</li>
             </ul>
