1b0087e48297a7ee175b9d54e9fea961b50e77b8
Javier <106848262+javiiicz@users.noreply.github.com>
2024-09-28 21:47:08 -0400

    Edit description

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -3,26 +3,8 @@ function Desc() {
       <div className="container flex flex-col items-center justify-center my-52 w-4/5 space-x-2">
           <div className="text flex-1 px-20 py-2">
             <p>
-              <span className="font-bold">Drowsy Driver</span> is a solution to the problem of 
-              <span className="font-bold"> drowsy driving</span>. Drowsy Driver helps you form 
-              better driving habits and stay alert on the road. Not only does it detect signs 
-              of fatigue, but it also collects data and offers insights to help you understand 
-              your driving patterns.
+            Introducing Drowsy Driver, an innovative software solution designed to enhance your camera's capabilities by monitoring driver fatigue in real-time. This cutting-edge application utilizes advanced facial recognition algorithms to analyze drivers' facial features and eye movements, detecting signs of drowsiness before it's too late. With instant alerts sent to the driver, Drowsy Driver ensures safer driving experiences and significantly reduces the risk of accidents caused by fatigue. Transform your standard camera into a powerful safety tool—make every journey safer with Drowsy Driver!
             </p>
-            <p>
-              This information can be used to identify areas for improvement and ultimately 
-              prevent accidents. Inspired by the prevalence of 
-              <span className="font-bold"> drowsy driving in Miami</span>, where we frequently 
-              witness people falling asleep at the wheel, Drowsy Driver aims to be a safety 
-              solution for drivers everywhere.
-            </p>
-          </div>
-          <div className="image flex-1 px-8 py-2 text-center">
-            <img
-              src="https://media.istockphoto.com/id/1155392792/photo/young-woman-driving-car-on-a-sunny-day.jpg?s=612x612&w=0&k=20&c=L6g-H0T5ldbMX5JUtug2AsoVRYzt7w5q9TUEPSQCu84="
-              alt="Image"
-              className="max-w-full h-auto rounded-lg"
-            />
           </div>
       </div>
     );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
         <>
             <div className="px-[5%] my-10 text-center">
                 <h2 className="text-5xl pt-[20%]">According to the National Highway Traffic Safety Administration more than 50000 car-related accidents occur anually due to drowsy driving.</h2>
-                <h2 className="text-4xl pt-24">What's the solution?</h2>
+                <h2 className="text-4xl pt-40">We can help.</h2>
             </div>
             
         </>
